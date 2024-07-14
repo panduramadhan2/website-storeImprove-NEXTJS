a79899a986001e87af9e8cd5cbaca00953cba63e
panduramadhan2 <panduramadhan2@gmail.com>
2024-07-14 22:22:04 +0700

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {
+//   images: {
+//     domains: ["res.cloudinary.com"],
+//   },
+// };
+
+// export default nextConfig;
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
