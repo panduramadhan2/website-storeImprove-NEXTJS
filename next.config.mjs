4c3390404e337f33dc21c00cebc720c9a0ef09aa
panduramadhan2 <panduramadhan2@gmail.com>
2024-07-15 15:32:29 +0700

    fixing deploy and ready

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,8 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["res.cloudinary.com"],
-//   },
-// };
-
-// export default nextConfig;
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+      domains: ["res.cloudinary.com"],
+    },
 };
 
 export default nextConfig;
